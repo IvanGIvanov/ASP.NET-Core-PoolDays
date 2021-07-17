afc691b120f74fdb6f63233c1e783fc56cd8078f
IvanGIvanov <ivanov.g.ivan@outlook.com>
2021-07-18 01:53:12 +0300

    Add "AddPoolFormModel" class, "PoolsController" class and "Add" view for adding/creating new pool in DB(Still not connected to the DB).

---
+++ PoolDays/Controllers/PoolsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoolDays.Models.Pools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoolDays.Controllers
+{
+    public class PoolsController : Controller
+    {
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddPoolFormModel pool)
+        {
+            return View();
+        }
+    }
+}
---
+++ PoolDays/Models/Pools/AddPoolFormModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoolDays.Models.Pools
+{
+    public class AddPoolFormModel
+    {
+        public string Manufacturer { get; init; }
+
+        public string Model { get; init; }
+
+        public string Description { get; init; }
+
+        public double Volume { get; init; }
+
+        public double Height { get; init; }
+
+        public double Width { get; init; }
+
+        public double Depth { get; init; }
+
+        public double? Diameter { get; init; }
+
+        public bool PumpIncluded { get; init; }
+
+        public bool Stairway { get; init; }
+
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; init; }
+
+        public int CategoryId { get; init; }
+    }
+}
