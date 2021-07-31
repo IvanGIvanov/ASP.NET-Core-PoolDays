1cefe2a797dec13f2666945f8751f279e902bbbf
IvanGIvanov <ivanov.g.ivan@outlook.com>
2021-07-31 23:45:04 +0300

    Implement basic API controller class to return info for all pools

---
+++ PoolDays/Controllers/Api/PoolsApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoolDays.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoolDays.Controllers.Api
+{
+    [ApiController]
+    [Route("api/pools")]
+    public class PoolsApiController : ControllerBase
+    {
+        private readonly PoolDaysDBContext data;
+
+        public PoolsApiController(PoolDaysDBContext data) => this.data = data;
+
+        [HttpGet]
+        public IEnumerable PoolGet()
+        {
+            return this.data.Pools.ToList();
+        }
+    }
+}
