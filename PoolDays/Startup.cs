8e703044177399643f3d098ef822211bb39346cf
IvanGIvanov <ivanov.g.ivan@outlook.com>
2021-08-02 23:43:56 +0300

    Add Anti forgery protection against CSRF for all project

--- PoolDays/Startup.cs
+++ PoolDays/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,7 +39,10 @@ namespace PoolDays
                 })
                 .AddEntityFrameworkStores<PoolDaysDBContext>();
             
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(options => 
+                options.
+                Filters.
+                Add(new AutoValidateAntiforgeryTokenAttribute()));
 
             services.AddTransient<IStatisticsService, StatisticsService>();
             services.AddTransient<IPoolService, PoolService>();
